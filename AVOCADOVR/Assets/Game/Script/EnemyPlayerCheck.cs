4ec1e00dbcfeb7e4121711f913efc9d9cc48c84e
KikukawaMakoto <m.kikukawa@avcd.co.jp>
2019-06-17 17:43:27 +0900

    ゲームScene追加(まだ、当たり判定とかはやっていない)

---
+++ AVOCADOVR/Assets/Game/Script/EnemyPlayerCheck.cs
@@ -0,0 +1,22 @@
+﻿/*
+*   Name:菊川 誠
+*   Script:敵のプレイヤーを検知する視野角のクラス
+*   Day:19/06/17 
+*/
+using UnityEngine;
+
+public class EnemyPlayerCheck : MonoBehaviour {
+    private EnemyStateManager m_EnemyStateManager;
+    //敵の視野角にプレイヤーが侵入した時
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            //もし、自分のEnemyStateManagerが検知できていなければ
+            if (!m_EnemyStateManager) {
+                //検知する
+                m_EnemyStateManager = transform.parent.gameObject.GetComponent<EnemyStateManager>();
+            }
+            //検知したScriptに情報を渡す。
+            m_EnemyStateManager.SetBattlePosture(other.transform);
+        }
+    }
+}
